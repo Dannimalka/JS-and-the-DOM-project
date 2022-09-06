6e8743c59a610f08ef588c04a8e0029ff9210995
Danni Malka <dannimalka.iag@gmail.com>
2022-09-06 19:31:16 +0300

    added a burger menu

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* adds elements to navbar and makes smooth scroll*/
 let ul = document.getElementById("navbar");
-
 const navSections = document.querySelectorAll("[data-nav]");
+
 navSections.forEach((individualSection) => {
   linkText = individualSection.getAttribute("data-nav");
   console.log(linkText);
@@ -14,7 +14,8 @@ navSections.forEach((individualSection) => {
   li.appendChild(text);
   ul.appendChild(li);
 });
-/*Not working as intended*/
+
+//Not working as intended
 /*
 const navUlLi = document.querySelectorAll("nav ul li");
 
@@ -27,12 +28,23 @@ window.addEventListener("scroll", function () {
       current = individualSection.getAttribute("data-nav");
     }
   });
-  navSections.forEach((individualSection) => {
-    if (individualSection) {
-      individualSection.className = "active";
-    } else {
-      individualSection.className = "";
+  navUlLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
     }
   });
 });
 */
+
+//burgermenu, all it does is to add a class of active to .burger when clicked on
+
+const navBurger = () => {
+  const burgerMenu = document.querySelector(".burger");
+  const navBar = document.querySelector(".navbar-ul");
+
+  burgerMenu.addEventListener("click", function () {
+    navBar.classList.toggle("burger-active");
+  });
+};
+navBurger();
---
+++ test.js
@@ -0,0 +1,107 @@
+/*not used in the actual code*/
+/*not used in the actual code*/
+/*not used in the actual code*/
+
+/*
+const colorchanger = document.querySelector("nav");
+navbar.addEventListener("click", function () {
+  console.log("this worked! clicked");
+  navbar.style.backgroundColor = "pink";
+*/
+/*
+function onScreen(section) {}
+*/
+
+/*
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains("scroll-down")
+  ) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
+});
+*/
+/*
+
+});
+*/
+/*
+function navBarUl() {
+  var ul = document.getElementById("navbar");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode("Showcase"));
+  ul.appendChild(li);
+}
+*/
+/*
+const body = document.querySelector("body");
+window.addEventListener("scroll", function () {
+  alert("please diseable addblocker!");
+  body.style.display = "none";
+});*/
+/*
+const title = document.querySelector("h1");
+const originalText = title.textContent;
+const updatedText = "testing this out!";
+
+title.addEventListener("mouseover", function () {
+  title.textContent = updatedText;
+});
+
+title.addEventListener("mouseout", function () {
+  title.textContent = originalText;
+});
+*/
+/*
+function navBarUl() {
+  ul.innerHTML += `<li> test </li>`;
+}
+*/
+
+/*
+let form = document.forms.subform,
+  elem = form.elements;
+console.log(elem);
+
+elem.submit.addEventListener("click", function (event) {
+  alert("input value is: " + elem.subject.value);
+});
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (!elem.subject.value) {
+    alert("please input text");
+    elem.subject.focus;
+  }
+  console.log("form submission succesful");
+  alert("thanks for testing");
+});
+*/
+
+/*
+document.addEventListener("scroll", function () {
+  navSections.forEach((individualSection, i) => {
+    if (onScreen(individualSection)) {
+      individualSection.className = "active";
+    } else {
+      individualSection.className = "";
+    }
+  });
+});
+
+*/
